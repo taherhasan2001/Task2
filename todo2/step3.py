376f9e16c42ad4121e8a2c10eb64d3ccc7d27a99
AlaaMurshed <104901665+AlaaMurshed@users.noreply.github.com>
2023-05-11 21:30:14 +0300

    Add files via upload

---
+++ todo2/step3.py
@@ -0,0 +1,47 @@
+class StringCalculator:
+    def add(self, numbers: str) -> int:
+        # if the input string is empty, return 0
+        if not numbers:
+            return 0
+
+        # Replace all occurrences of '\n' with ','
+        numbers = numbers.replace('\n', ',')
+
+        # split the input string into parts using the comma as a delimiter
+        parts = numbers.split(",")
+
+        # initialize a variable to hold the sum
+        sum = 0
+
+        # iterate over the parts and add them to the sum
+        for part in parts:
+            sum += int(part)
+
+        # return the sum
+        return sum
+
+
+
+# test the "add" method with an empty string as input
+def test_add_empty_string():
+    calculator = StringCalculator()
+    assert calculator.add("") == 0
+
+# test the "add" method with a single number as input
+def test_add_single_number():
+    calculator = StringCalculator()
+    assert calculator.add("5") == 5
+
+# test the "add" method with two numbers as input
+def test_add_two_numbers():
+    calculator = StringCalculator()
+    assert calculator.add("2,3") == 5
+
+# test the "add" method with unknown amount of numbers as input
+def test_add_an_unknown_amount_of_numbers():
+    calculator = StringCalculator()
+    assert calculator.add("2,3,4,1") == 10
+
+def test_to_handle_new_lines_between_numbers():
+    calculator = StringCalculator()
+    assert calculator.add("2\n3,4,1") == 10
